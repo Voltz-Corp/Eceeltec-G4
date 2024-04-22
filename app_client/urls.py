b626c7e0cf14fef610a31395bac4d946920db35a
Gabriel Albuquerque <gabrielmca2004@gmail.com>
2024-04-22 17:56:48 -0300

    starting creating orders, and error treatment

--- app_client/urls.py
+++ app_client/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'client'
 urlpatterns = [
     path('', views.OrderViewView.as_view(), name="view_orders"),
+    path('iniciar_pedido/', views.RequestOrderView.as_view(), name="Request_OS"),
+
 ]
--- app_client/views.py
+++ app_client/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.views import View
-# from django.contrib.auth.hashers import make_password
-from .models import Order
-# from rolepermissions.decorators import has_permission_decorator
-# from django.utils.decorators import method_decorator
+from django.contrib.auth.hashers import make_password
+from .models import OrderRequest
+from rolepermissions.decorators import has_permission_decorator
+from django.utils.decorators import method_decorator
 from django.shortcuts import render, get_object_or_404
+from .utils import Product_verify
 
 
 #falta o login
@@ -17,5 +18,22 @@ class OrderViewView(View):
         #     'app_name': 'orders',
         #     'user': request.user,
         # }
-        return render(request, 'orders.html')
+        return render(request, 'RequestOrder/orders.html')
+
+class RequestOrderView(View):
+    def get(self,request):
+
+        return render(request, 'RequestOrder/create-OS.html')
+    def post(self, request):
+        productBrand = request.POST.get('brand')
+        productType = request.POST.get('type')
+        productModel = request.POST.get('model')
+        productOther = request.POST.get('other')
+        productDescription = request.POST.get('description')
+
+        user_id = request.user.id
+
+        product_list = []
+
+        errors = Product_verify( productBrand, productType, productModel, productOther, productDescription)
 
