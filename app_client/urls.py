ea0146ce12f2afd44df1ea5be500d3c9d1fd2c9b
Paulo Rosado <paulohenrique110803@gmail.com>
2024-04-23 18:42:06 -0300

    fixing urls

--- app_client/urls.py
+++ app_client/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'client'
 urlpatterns = [
     path('', views.OrderViewView.as_view(), name="view_orders"),
-    path('registrar', views.SignUpClient.as_view(), name="sign_up"),
-    path('login', views.SignInView.as_view(), name="sign_in"),
-    path('iniciar_pedido/', views.RequestOrderView.as_view(), name="request_os"),
+    path('cadastro/', views.SignUpClient.as_view(), name="sign_up"),
+    path('login/', views.SignInView.as_view(), name="sign_in"),
+    path('solicitar_servico/', views.RequestOrderView.as_view(), name="request_os"),
 ]
