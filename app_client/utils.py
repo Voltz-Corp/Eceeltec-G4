ffb8a49dcbcb82f9d1e8c585f850e1ce22999f9d
Gabriel Albuquerque <gabrielmca2004@gmail.com>
2024-04-23 16:36:01 -0300

    making some changes on css and util from client

--- app_client/utils.py
+++ app_client/utils.py
@@ -8,9 +8,43 @@ def product_verify(brand, type, model, other, description):
             'field': 'other',
             'message' : 'Este campo não pode ser vazio!'
             })
-    elif 2 > len(type) or len(type) > 50:
+    elif 2 > len(other) or len(other) > 75:
         errors.append({
             'field':'other',
             'message': 'Insira de 2 a 75 caracteres!'
             })
+    if 2 > len(model) or len(model) > 75:
+        errors.append({
+            'field':'model',
+            'message': 'Insira de 2 a 75 caracteres!'
+            })
+    if str(brand).count(' ') == len(brand):
+        errors.append({
+            'field':'brand',
+            'message': 'Este campo não pode ser vazio!'
+            })
+    elif 2 > len(brand) or len(brand) > 75:
+        errors.append({
+            'field':'brand',
+            'message': 'Insira de 2 a 75 caracteres!'
+            })
+    if str(description).count(' ') == len(description):
+        errors.append({
+            'field':'description',
+            'message': 'Este campo não pode ser vazio!'
+            })
+    elif 2 > len(description) or len(description) > 200:
+        errors.append({
+            'field':'description',
+            'message': 'Insira de 2 a 200 caracteres!'
+            })
+    
+    if len(errors) > 0:
+        return errors
 
+    product = OrderRequest(productbrand = brand, productType = type, productModel = model, otherProductType = other, productProblemDescription = description)
+    product.save()
+    return  product
+
+
+        
