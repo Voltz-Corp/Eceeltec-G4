7178ac72fc2da4d60ba77c038c167743650b5691
sophia <deletrasopa@gmail.com>
2024-04-24 08:55:06 -0300

    adding phone and cep regex

--- app_client/static/js/cep.js
+++ app_client/static/js/cep.js
@@ -6,7 +6,9 @@ const address = document.querySelector('#address');
 
 
 async function handleZipCodeChange(zip) {
-  if (zip.length === 8) {
+    const formattedZipCode = zip.split('-').join('');
+
+    if (formattedZipCode.length === 8) {
     try {
       const response = await fetch(
         `https://viacep.com.br/ws/${zip}/json/`,
@@ -25,4 +27,10 @@ async function handleZipCodeChange(zip) {
 
 cep?.addEventListener('blur', async () => {
   await handleZipCodeChange(cep.value);
+})
+
+cep.addEventListener('input', (event) => {
+  const formattedCep = event.target.value.replace(/^(\d{5})(\d{3})$/, '$1-$2').substring(0, 9)
+
+  cep.value = formattedCep;
 })
