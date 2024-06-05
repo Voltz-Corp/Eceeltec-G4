1a06703c5a619e9893b60c2400c3f0c06a624d86
deadcube04 <gmca@cesar.school>
2024-06-05 14:19:09 -0300

    uncommenting test

--- cypress/e2e/GerenciamentodDeSolicitações.cy.js
+++ cypress/e2e/GerenciamentodDeSolicitações.cy.js
@@ -61,62 +61,62 @@ Cypress.Commands.add('CreateSolicitation', () => {
 })
 
   describe('home page', () => {
-    // it('Definindo data com sucesso', () => {
-    //     cy.exec('python manage.py migrate')
-    //     cy.DeleteAndCreateAdm()
-    //     cy.visit('/')
-    //     cy.on("uncaught:exception", (e, runnable) => {
-    //         console.log("error", e);
-    //         console.log("runnable", runnable);
-    //         console.log("error", e.message);
-    //         return false;
-    //         });
+    it('Definindo data com sucesso', () => {
+        cy.exec('python manage.py migrate')
+        cy.DeleteAndCreateAdm()
+        cy.visit('/')
+        cy.on("uncaught:exception", (e, runnable) => {
+            console.log("error", e);
+            console.log("runnable", runnable);
+            console.log("error", e.message);
+            return false;
+            });
 
-    //     cy.CreateSolicitation()
-    //     cy.visit('/')
-    //     cy.ClientLogout()
-    //     cy.CreateAdmin()
-    //     cy.get(':nth-child(1) > a').click()
-    //     cy.get(':nth-child(6) > a').click()
-    //     cy.get('.scheduleDateContainer > input').invoke('removeAttr', 'type').type('2024-06-18')
-    //     cy.get('.content > form > button').click()
-    //     cy.get('.logout > button').click()
-    //     cy.GoToClient()
-    //     cy.get('tbody > tr > :nth-child(3)').invoke('text').should('have.string', "18 de Junho de 2024")
-    //     cy.get('.AGENDADO').invoke('text').should('have.string', 'Agendado')
+        cy.CreateSolicitation()
+        cy.visit('/')
+        cy.ClientLogout()
+        cy.CreateAdmin()
+        cy.get(':nth-child(1) > a').click()
+        cy.get(':nth-child(6) > a').click()
+        cy.get('.scheduleDateContainer > input').invoke('removeAttr', 'type').type('2024-06-18')
+        cy.get('.content > form > button').click()
+        cy.get('.logout > button').click()
+        cy.GoToClient()
+        cy.get('tbody > tr > :nth-child(3)').invoke('text').should('have.string', "18 de Junho de 2024")
+        cy.get('.AGENDADO').invoke('text').should('have.string', 'Agendado')
 
-    //  })
+     })
 
-    // it('Definindo orçamento com sucesso', () => {
-    //     cy.exec('python manage.py migrate')
-    //     cy.DeleteAndCreateAdm()
-    //     cy.visit('/')
-    //     cy.on("uncaught:exception", (e, runnable) => {
-    //         console.log("error", e);
-    //         console.log("runnable", runnable);
-    //         console.log("error", e.message);
-    //         return false;
-    //         });
+    it('Definindo orçamento com sucesso', () => {
+        cy.exec('python manage.py migrate')
+        cy.DeleteAndCreateAdm()
+        cy.visit('/')
+        cy.on("uncaught:exception", (e, runnable) => {
+            console.log("error", e);
+            console.log("runnable", runnable);
+            console.log("error", e.message);
+            return false;
+            });
 
-    //     cy.CreateSolicitation()
-    //     cy.visit('/')
-    //     cy.ClientLogout()
-    //     cy.CreateAdmin()
-    //     cy.get(':nth-child(1) > a').click()
-    //     cy.get(':nth-child(6) > a').click()
-    //     cy.get('.scheduleDateContainer > input').invoke('removeAttr', 'type').type('2024-06-18')
-    //     cy.get('.content > form > button').click()
-    //     cy.get('#status').select('Aguardando orçamento')
-    //     cy.get('.content > form > button').click()
-    //     cy.get('.budgetContainer > input').type('50')
-    //     cy.get('.content > form > button').click()
-    //     cy.get('.logout > button').click()
-    //     cy.GoToClient()
-    //     cy.get(':nth-child(4) > a').click()
-    //     cy.get('.price > span').invoke('text').should('have.string', "50,00")
-    //     cy.get('.waitingForm > p').invoke('text').should('have.string', "Você deseja prosseguir com o serviço?")
+        cy.CreateSolicitation()
+        cy.visit('/')
+        cy.ClientLogout()
+        cy.CreateAdmin()
+        cy.get(':nth-child(1) > a').click()
+        cy.get(':nth-child(6) > a').click()
+        cy.get('.scheduleDateContainer > input').invoke('removeAttr', 'type').type('2024-06-18')
+        cy.get('.content > form > button').click()
+        cy.get('#status').select('Aguardando orçamento')
+        cy.get('.content > form > button').click()
+        cy.get('.budgetContainer > input').type('50')
+        cy.get('.content > form > button').click()
+        cy.get('.logout > button').click()
+        cy.GoToClient()
+        cy.get(':nth-child(4) > a').click()
+        cy.get('.price > span').invoke('text').should('have.string', "50,00")
+        cy.get('.waitingForm > p').invoke('text').should('have.string', "Você deseja prosseguir com o serviço?")
 
-    //  })
+     })
   
       it('inserindo data inválida', () => {
         cy.exec('python manage.py migrate')
