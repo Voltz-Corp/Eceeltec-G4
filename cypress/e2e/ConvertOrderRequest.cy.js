26c5af9f8056febd3fd3d375ba69801c29bb6a3c
kat0 <lhk@cesar.school>
2024-06-07 17:39:33 -0300

    removed third cenario from h3

--- cypress/e2e/ConvertOrderRequest.cy.js
+++ cypress/e2e/ConvertOrderRequest.cy.js
@@ -86,43 +86,4 @@ describe('HomePage', () => {
         cy.get('#detailed_problem_description').invoke('text').should('have.string', "O compressor queimou")
         cy.get('#necessary_parts').invoke('text').should('have.string', "1x Compressor Tecumseh 1/4 HP 60Hz 110V")
   })
-  it('Campos Vazios', () => {
-    cy.exec('python manage.py migrate')
-        cy.DeleteAndCreateAdm()
-        cy.visit('/')
-        cy.on("uncaught:exception", (e, runnable) => {
-            console.log("error", e);
-            console.log("runnable", runnable);
-            console.log("error", e.message);
-            return false;
-            });
-
-        cy.CreateClient("Luan Kato", "kato@gmail.com")
-        cy.CreateSolicitation()
-        cy.visit('/')
-        cy.ClientLogout()
-        cy.CreateAdmin()
-        cy.get(':nth-child(1) > a').click()
-        cy.get(':nth-child(6) > a').click()
-        cy.get('.scheduleDateContainer > input').invoke('removeAttr', 'type').type('2024-06-18')
-        cy.get('.content > form > button').click()
-        cy.get('#status').select('Aguardando orçamento')
-        cy.get('.content > form > button').click()
-        cy.get('.budgetContainer > input').type('50')
-        cy.get('.content > form > button').click()
-        cy.get('.logout > button').click()
-        cy.GoToClient("kato@gmail.com")
-        cy.get('.view > button').click()
-        cy.get('.yes')
-        cy.get('.waitingForm > form > button').click()
-        cy.ClientLogout()
-        cy.ChangeToAdmin()
-        cy.get(':nth-child(1) > a').click()
-        cy.get(':nth-child(6) > a').click()
-        cy.get('#detailed_problem_description').type(' ')
-        cy.get('#necessary_parts').type(' ')
-        cy.get('.works > button').click()
-        
-        
-  })
 })
