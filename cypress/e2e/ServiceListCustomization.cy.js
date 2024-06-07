784e85ddab6fadebf2c35a75f7ebef37b258cef4
viniciusdandrade <vaj@cesar.school>
2024-06-06 21:25:22 -0300

    starting two scenarios

--- cypress/e2e/ServiceListCustomization.cy.js
+++ cypress/e2e/ServiceListCustomization.cy.js
@@ -1,5 +1,5 @@
 describe('Service filtering test', () =>{
-    it('successfully filtering services as client', () => {
+    it('successfully filtering services as employee', () => {
         cy.exec('python manage.py migrate')
         cy.DeleteAndCreateAdm()
         cy.visit('/')
@@ -34,27 +34,27 @@ describe('Service filtering test', () =>{
         cy.GoToClient()
     })
 
-    // it('successfully filtering services as admin', () => {
-    //     cy.exec('python manage.py migrate')
-    //     cy.DeleteAndCreateAdm()
-    //     cy.visit('/')
-    //     cy.on("uncaught:exception", (e, runnable) => {
-    //         console.log("error", e);
-    //         console.log("runnable", runnable);
-    //         console.log("error", e.message);
-    //         return false;
-    //         });
-    // })
+    it('successfully filtering services as admin', () => {
+        cy.exec('python manage.py migrate')
+        cy.DeleteAndCreateAdm()
+        cy.visit('/')
+        cy.on("uncaught:exception", (e, runnable) => {
+            console.log("error", e);
+            console.log("runnable", runnable);
+            console.log("error", e.message);
+            return false;
+            });
+        })
 
-    // it('successfully filtering services as employee', () => {
-    //     cy.exec('python manage.py migrate')
-    //     cy.DeleteAndCreateAdm()
-    //     cy.visit('/')
-    //     cy.on("uncaught:exception", (e, runnable) => {
-    //         console.log("error", e);
-    //         console.log("runnable", runnable);
-    //         console.log("error", e.message);
-    //         return false;
-    //         });
-    // })
+    it('successfully filtering services as client', () => {
+        cy.exec('python manage.py migrate')
+        cy.DeleteAndCreateAdm()
+        cy.visit('/')
+        cy.on("uncaught:exception", (e, runnable) => {
+            console.log("error", e);
+            console.log("runnable", runnable);
+            console.log("error", e.message);
+            return false;
+            });
+    })
 })
